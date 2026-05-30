0d4429d903d4d8b1b2643420aa914bf4aacf20bd
jasleenkaur1014 <jasleenkaur1014@gmail.com>
2026-05-31 00:56:21 +0530

    Basic commit

--- src/data.jsx
+++ src/data.jsx
@@ -17,22 +17,4 @@ export const initialFriends = [
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
-  {
-    id: 765432,
-    name: "Ishi",
-    image: "https://i.pravatar.cc/48?u=765432",
-    balance: 30,
-  },
-  {
-    id: 987654,
-    name: "Miru",
-    image: "https://i.pravatar.cc/48?u=987654",
-    balance: 10,
-  },
-  {
-    id: 933923,
-    name: "Keyan",
-    image: "https://i.pravatar.cc/48?u=933923",
-    balance: 0,
-  },
 ];
